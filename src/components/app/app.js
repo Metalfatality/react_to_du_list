9f473fa7f9da5b0ea4163381dad9f86e36ad3495
Metalfatality <argentum@nextmail.ru>
2021-10-23 21:52:24 +0300

    increased, employees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -46,17 +46,6 @@ class App extends Component {
     }
 
     onToggleIncrease = (id) => {
-        // this.setState(({data}) => {
-        //     const index = data.findIndex(elem => elem.id === id);
-
-        //     const old = data[index];
-        //     const newItem = {...old, increase: !old.increase};
-        //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
-
-        //     return {
-        //         data: newArr
-        //     }
-        // })
         this.setState(({data}) => ({
             data: data.map(item => {
                 if (item.id === id) {
@@ -72,9 +61,11 @@ class App extends Component {
     }
 
     render() {
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
         return (
             <div className="app">
-                <AppInfo />
+                <AppInfo employees={employees} increased = {increased}/>
     
                 <div className="search-panel">
                     <SearchPanel/>
